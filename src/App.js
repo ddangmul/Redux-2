51d7b0b5a9e346ecc455923a56070f0fd1da1c62
ddangmul <j.ddangmul@gmail.com>
2024-12-29 01:23:37 +0900

    리덕스로 http 상태 관리 - uiSlice에 notification 상태 추가, App 로딩 시 첫 장바구니 빈 배열 전송 방지

--- src/App.js
+++ src/App.js
@@ -1,31 +1,83 @@
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Fragment, useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+// 앱 로딩시 빈 장바구니 백엔드에 전송 방지: 함수 외부에 변수 선언
+let initial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   // 전체 장바구니 상태에 대한 구독 설정 -> cart 변경 시 App 재실행 & cart 최신 상태
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   // [ 리덕스 상태에 따라 달라지는 부수 효과 ]
   useEffect(() => {
-    fetch(
-      "https://react-redux-http-cf0d8-default-rtdb.firebaseio.com/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-redux-http-cf0d8-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]); // 의존성에 cart : cart 변경될 때마다 fetch
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (initial) {
+      // 초기값인 true이면 HTTP 요청 함수 sendCartData() 방지
+      initial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Senting cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]); // 의존성에 cart : cart 변경될 때마다 fetch
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -3,11 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       // 리덕스 툴킷이 코드를 캡쳐해 제3의 라이브러리 imer로 새 상태 객체 생성
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
