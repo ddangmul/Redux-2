8f0acaa7f990f89988ec074f637e87340a71d64e
ddangmul <j.ddangmul@gmail.com>
2024-12-29 00:52:54 +0900

    firebase 연동 & useEffect로  백엔드 서버에 cart 데이터 쓰기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -6,6 +7,19 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  // 전체 장바구니 상태에 대한 구독 설정 -> cart 변경 시 App 재실행 & cart 최신 상태
+  const cart = useSelector((state) => state.cart);
+
+  // [ 리덕스 상태에 따라 달라지는 부수 효과 ]
+  useEffect(() => {
+    fetch(
+      "https://react-redux-http-cf0d8-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]); // 의존성에 cart : cart 변경될 때마다 fetch
 
   return (
     <Layout>
